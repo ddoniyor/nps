7186eebe662da3bfe0a1bd94da680c1e8d1d323e
Doniyor Ganiev <donik.ganiev05@mail.ru>
2020-01-04 09:24:38 +0300

    refactoring: add variables for nps

--- main.go
+++ main.go
@@ -4,32 +4,41 @@ import "fmt"
 //shift +enter перевод строки
 //main + Tag - live templates
 func main() {
+
 	score1:=10
-	score2:=10
+	score2:=7
 	score3:=10
 
 	promoters :=0
 	detractors :=0
 
-	if score1 >= 9{
+	//refactoring - улучшение структуры кода без модификации поведения
+	//ctrl alt v - позволять менять константы
+	//shift fn f6
+
+	promoutersLowerBound := 9
+	detractorsUpperBound := 6
+
+	if score1 >= promoutersLowerBound {
 		promoters++
 	}
 
-	if score1 <= 6{
+
+	if score1 <= detractorsUpperBound {
 		detractors++
 	}
-	if score2 >= 9{
+	if score2 >= promoutersLowerBound {
 		promoters++
 	}
 
-	if score2 <= 6{
+	if score2 <= detractorsUpperBound {
 		detractors++
 	}
-	if score3 >= 9{
+	if score3 >= promoutersLowerBound {
 		promoters++
 	}
 
-	if score3 <= 6{
+	if score3 <= detractorsUpperBound {
 		detractors++
 	}
 	nps:=(promoters-detractors)*100/3
